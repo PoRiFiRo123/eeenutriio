2a9ce0e7020fdc25c6e24ffcb7b30e527965a407
nishitkirani2020 <nishitkirani2020@gmail.com>
2024-08-28 04:45:04 +0000

    Update ErrorPage.jsx

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -26,7 +26,7 @@ const ErrorPage = () => {
                     </div>
                     <div className="col-lg-8 col-sm-6 col-12">
                         <div className="foue-zero-thumb">
-                            <img src="/src/assets/images/404.png" alt="Nutriio" />
+                            <img src="https://i.imgur.com/hIHYZEL.png" alt="Nutriio" />
                         </div>
                     </div>
                 </div>
